3701ae46a8d2d84e01c487a99d7b5f944c7a279f
Leszek Klich <leszek.klich@gmail.com>
2018-12-28 13:34:46 +0100

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zbiór atrybutów. Zmień wartości tych atrybutów by zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("LKLogArchiver")]
+[assembly: AssemblyDescription("Program do archiwizacji logów Windows")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Leszek Klich")]
+[assembly: AssemblyProduct("LKLogArchiver")]
+[assembly: AssemblyCopyright("Copyright © Leszek Klich 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie wartości ComVisible na false sprawia, że typy w tym zestawie nie będą widoczne 
+// dla składników COM.  Jeśli potrzebny jest dostęp do typu w tym zestawie z 
+// COM, ustaw wartość ComVisible na true, dla danego typu.
+[assembly: ComVisible(false)]
+
+// Następujący GUID jest dla ID typelib jeśli ten projekt jest dostępny dla COM
+[assembly: Guid("05bd9277-b66f-476a-98bb-14d0034baf98")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza 
+//      Numer kompilacji
+//      Rewizja
+//
+// Można określać wszystkie wartości lub używać domyślnych numerów kompilacji i poprawki 
+// poprzez użycie '*', jak pokazane jest poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
